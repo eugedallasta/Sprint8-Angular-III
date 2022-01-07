14586ed2ab074df70890551984d526ea8191b1c7
Euge Dallasta <mariaedallasta@gmail.com>
2022-01-07 14:30:45 +0100

    add components

--- starWarsApp/src/app/app.module.ts
+++ starWarsApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { PilotComponent } from './components/pilot/pilot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CardComponent,
+    HomeComponent,
+    NavBarComponent,
+    LoginComponent,
+    PilotComponent
   ],
   imports: [
     BrowserModule,
